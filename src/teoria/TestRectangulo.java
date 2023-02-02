1e1522b39efc345831e1a569d91f74e009d33d92
manuel <programacionvirgendelcarmen@gmail.com>
2023-02-02 13:44:41 +0100

    Ejercicio Rectangulos después de refactorizar

--- src/teoria/TestRectangulo.java
+++ src/teoria/TestRectangulo.java
@@ -1,5 +1,6 @@
 package teoria;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TestRectangulo {
@@ -8,11 +9,13 @@ public class TestRectangulo {
 
         int numeroRectangulos = solicitarAUsuarioNumRectangulos();
         Rectangulo[] rectangulos = new Rectangulo[numeroRectangulos];
-        for (int i = 0; i < numeroRectangulos; i++) {
-            Rectangulo rectangulo = crearRectangulo();
-            rectangulos[i] = rectangulo;
+        System.out.println(Arrays.toString(rectangulos)); //vemos los datos iniciales
+        SCANNER.nextLine();
+        for (int i = 0; i < rectangulos.length; i++) {
+            rectangulos[i] = crearRectangulo();
         }
         mostrarRectangulos(rectangulos);
+        SCANNER.close();
     }
 
     private static void mostrarRectangulos(Rectangulo[] rectangulos) {
@@ -20,32 +23,38 @@ public class TestRectangulo {
             System.out.println(rectangulo.toString());
         }
     }
-
-    private static int solicitarAUsuarioNumRectangulos() {
-        String sNumeroRectangulos = "";
+    private static int devolverEnteroRegex(String mensaje, String regex) {
+        String sNumero = "";
         do {
-            System.out.println("Indica el nº de rectángulos a crear (2-15) inclusive");
-            sNumeroRectangulos = SCANNER.nextLine();
-        } while (!sNumeroRectangulos.matches("[2-9]|1[0-5]"));
-        int iNumeroRectangulos = Integer.parseInt(sNumeroRectangulos);
+            System.out.println(mensaje);
+            sNumero = SCANNER.nextLine();
+        } while (!sNumero.matches(regex));
+        int iNumero = Integer.parseInt(sNumero);
+        return iNumero;
+    }
+    private static int solicitarAUsuarioNumRectangulos() {
+        String mensaje = "Indica el nº de rectángulos a crear (2-15) inclusive";
+        String regex = "[2-9]|1[0-5]";
+        int iNumeroRectangulos = devolverEnteroRegex(mensaje, regex);
         return iNumeroRectangulos;
     }
 
     private static Rectangulo crearRectangulo() {
+
         Rectangulo rectangulo = new Rectangulo();
-        String sAlto = "", sAncho = "";
-        do {
-            System.out.println("Dame alto del rectángulo (1-25) inclusive");
-            sAlto = SCANNER.nextLine();
-        } while (! sAlto.matches("[1-9]|1[0-9]|2[0-5]"));
-        int iAlto = Integer.parseInt(sAlto);
-        do {
-            System.out.println("Dame ancho del rectángulo (1-60) inclusive");
-            sAncho = SCANNER.nextLine();
-        } while (! sAncho.matches("[1-9]|[1-5][0-9]|60"));
-        int iAncho = Integer.parseInt(sAncho);
+        System.out.printf("Rectángulo inicial: %s%n", rectangulo);
+
+        String mensaje = "Dame alto del rectángulo (1-25) inclusive";
+        String regex = "[1-9]|1[0-9]|2[0-5]";
+        int iAlto = devolverEnteroRegex(mensaje, regex);
+
+        mensaje = "Dame ancho del rectángulo (1-60) inclusive";
+        regex = "[1-9]|[1-5][0-9]|60";
+        int iAncho = devolverEnteroRegex(mensaje, regex);
+
         rectangulo.setAlto(iAlto);
         rectangulo.setAncho(iAncho);
+
         return rectangulo;
     }
 }
